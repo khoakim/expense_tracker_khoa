a139f97ca6f71fe5cdb1bdcf1f3afcefcce5e052
Khoa Kim <khoa.kim85@gmail.com>
2021-11-09 19:20:47 +1100

    progressing

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,12 +1,98 @@
-import React, { useContext } from 'react';
-import { Transaction } from './Transaction';
-import { View, Text, StyleSheet, FlatList} from 'react-native';
+import React, { useContext, useState } from "react";
+import { Transaction } from "./Transaction";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
-import { GlobalContext } from '../context/GlobalContext';
+import { GlobalContext } from "../context/GlobalContext";
+import { COLORS, FONTS, SIZES, icons, images } from "../constants";
 
+import data from "../data/mockData";
 const TransactionList = () => {
+  const [categories, setCategories] = React.useState(data);
+  const [selectedCategory, setSelectedCategory] = React.useState(null);
   const { transactions } = useContext(GlobalContext);
-  // console.log(`transactions`, transactions);
+
+  console.log(`transactions`, transactions);
+
+  // label: `${percentage}%`,
+  // y: Number(item.y),
+  // expenseCount: item.expenseCount,
+  // color: item.color,
+  // name: item.name,
+  // id: item.id,
+  function setSelectCategoryByName(name) {
+    let category = categories.filter(a => a.name == name);
+    setSelectedCategory(category[0]);
+  }
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={{
+        flexDirection: "row",
+        height: 40,
+        paddingHorizontal: SIZES.radius,
+        borderRadius: 10,
+        backgroundColor:
+          selectedCategory && selectedCategory.name == item.name
+            ? item.color
+            : COLORS.white,
+      }}
+      onPress={() => {
+        let categoryName = item.name;
+        setSelectCategoryByName(categoryName);
+      }}
+    >
+      {/* Name/Category */}
+      <View style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
+        <View
+          style={{
+            width: 20,
+            height: 20,
+            backgroundColor:
+              selectedCategory && selectedCategory.name == item.name
+                ? COLORS.white
+                : item.color,
+            borderRadius: 5,
+          }}
+        />
+
+        <Text
+          style={{
+            marginLeft: SIZES.base,
+            color:
+              selectedCategory && selectedCategory.name == item.name
+                ? COLORS.white
+                : COLORS.primary,
+            ...FONTS.h3,
+          }}
+        >
+          {item.name}
+        </Text>
+      </View>
+
+      {/* Expenses */}
+      <View style={{ justifyContent: "center" }}>
+        <Text
+          style={{
+            color:
+              selectedCategory && selectedCategory.name == item.name
+                ? COLORS.white
+                : COLORS.primary,
+            ...FONTS.h3,
+          }}
+        >
+          {/* {item.y} AUD - {item.label} */}
+          asdadasdsa
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
     <View>
       <Text h3>History</Text>
@@ -14,26 +100,38 @@ const TransactionList = () => {
         {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul> */}
 
-      <FlatList
-        data={transactions}
-        renderItem={ ({item}) => {
-          return (
-            <View style={styles.list}>
-              <Text>{item.text}   </Text>
-            <Text>{item.amount}</Text>
+      <View>
+        <FlatList
+          data={transactions}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.list}>
+                <Text>{item.text} </Text>
+                <Text>{item.amount}</Text>
               </View>
-          )
-        }}
-        keyExtractor={ (transaction) => transaction.id}
-      />
+            );
+          }}
+          keyExtractor={transaction => transaction.id}
+        />
+      </View>
+
+      <View style={{ padding: SIZES.padding, width: 300, marginTop: 100 }}>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => `${item.id}`}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  list : {
-    flexDirection: 'row'
-  }
-})
+  list: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default TransactionList;
---
+++ src/constants/icons.js
@@ -0,0 +1,48 @@
+// export const baby_car = require("../../assets/icons/baby_car_icon.png");
+// export const back_arrow = require("../../assets/icons/back_arrow_icon.png");
+// export const calendar = require("../../assets/icons/calendar_icon.png");
+// export const chart = require("../../assets/icons/chart_icon.png");
+// export const cloth_icon = require("../../assets/icons/cloth_icon.png");
+// export const down_arrow = require("../../assets/icons/down_arrow.png");
+// export const education = require("../../assets/icons/education_icon.png");
+// export const food = require("../../assets/icons/food_icon.png");
+// export const healthcare = require("../../assets/icons/healthcare_icon.png");
+// export const menu = require("../../ssets/icons/menu_icon.png");
+// export const more = require("../../assets/icons/more_icon.png");
+// export const pin = require("../../assets/icons/pin.png");
+// export const sports_icon = require("../../assets/icons/sports_icon.png");
+// export const upr_arrow = require("../../assets/icons/up_arrow.png");
+
+// export default {
+//   baby_car,
+//   back_arrow,
+//   calendar,
+//   chart,
+//   cloth_icon,
+//   down_arrow,
+//   education,
+//   food,
+//   healthcare,
+//   menu,
+//   more,
+//   pin,
+//   sports_icon,
+//   up_arrow,
+// };
+
+const icons = {
+  // profile: {
+  //     profile: require('./profile/profile.png'),
+  //     comments: require('./profile/comments.png'),
+  // },
+  // image1: require('./image1.jpg'),
+  // image2: require('./image2.jpg'),
+  education: require("../../assets/icons/education_icon.png"),
+  baby_car: require("../../assets/icons/baby_car_icon.png"),
+  food: require("../../assets/icons/food_icon.png"),
+  healthcare: require("../../assets/icons/healthcare_icon.png"),
+  sports_icon: require("../../assets/icons/sports_icon.png"),
+  cloth_icon: require("../../assets/icons/cloth_icon.png"),
+};
+
+export default icons;
